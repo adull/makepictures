61c40c2df3e8a208a79684b7bb3719a7a5db2ad8
adull <adlaiabdelrazaq@gmail.com>
2017-07-19 09:43:42 -0500

    half working brush shit

--- js/brushdefine.js
+++ js/brushdefine.js
@@ -1,41 +1,64 @@
 
 
 function onMouseDown(event) {
-  if(!startingPoint) {
+  if(startingPoint == "notinitialized") {
     startingPoint = event.point;
     var tempArr = [];
     tempArr.push(0)
     tempArr.push(0)
     brushDefinePos.push(tempArr)
+    console.log("new starting Point")
+
   }
-  path3 = new Path({
+  brushPath = new Path({
     segments: [event.point],
-    strokeColor: 'black',
+    strokeColor: currentColor,
     closed: false
   });
+  setMode("brush")
 
 }
+$("#brush-define-wrapper").on('mouseenter', function() {
+  $("#brush-define-trash").css("opacity", "1");
+});
+$("#brush-define-wrapper").on('mouseleave', function() {
+  $("#brush-define-trash").css("opacity", "0");
+})
+$('#brush-define-trash').on('click', function() {
+  console.log("trigga")
+  brushDefineShape = [];
+  var canvas = $('#brush-define')[0];
+  var context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  // brushDefineShape[0] = [];
+  for(i = 0; i < brushDefineShape.length; i ++ ) {
+    brushDefineShape[i] = [];
+  }
+  brushPath.segments = [];
+  brushDefineTempArr = [];
+  startingPoint = "notinitialized";
+})
+
 function onMouseDrag(event) {
-    path3.add(event.point);
+    brushPath.add(event.point);
     canvas = $('#brush-define');
     var dataURL = canvas[0].toDataURL();
     $('#copy-img').attr("src",dataURL);
-    var tempArr = [];
-    tempArr.push(startingPoint.x - event.point.x);
-    tempArr.push(startingPoint.y - event.point.y);
-    brushDefinePos.push(tempArr);
+    brushDefineTempArr = [];
+    brushDefineTempArr.push(startingPoint.x - event.point.x);
+    brushDefineTempArr.push(startingPoint.y - event.point.y);
+    brushDefinePos.push(brushDefineTempArr);
 }
 function onMouseUp(event) {
-  // console.log(path3.segments)
-  // console.log(brushDefineShape);
-  // brushDefineShape.push(path3.segments);
-  // console.log(brushDefineShape[0])
-  brushDefineShape = path3.segments;
+  brushDefineTempArr = brushPath.segments;
+  brushDefineShape.push(brushDefineTempArr);
+  console.log("brushDefineShape after mouseup brushdefine")
+  console.log(brushDefineShape)
 }
 
 function onKeyDown(event) {
   if(event.key == 'k') {
-    path3.fillColor = 'red';
+    brushPath.fillColor = currentColor;
   }
   if(event.key == 'g') {
     console.log(brushDefineShape);
--- js/circlaoalalaka.js
+++ js/circlaoalalaka.js
@@ -1,3 +1,57 @@
-var startingPoint;
+//GLOBAL SHIT
+var currentColor = 'black'
+var startingPoint = "notinitialized";
+var brushPath;
+var brushDefineTempArr;
 var brushDefineShape = [];
 var brushDefinePos = [];
+
+//MODE
+var brushMode = true;
+var rectangleMode = false;
+var triangleMode = false;
+var circleMode = false;
+var lineMode = false;
+
+
+function setMode(shape) {
+  brushMode = false;
+  rectangleMode = false;
+  triangleMode = false;
+  circleMode = false;
+  lineMode = false;
+  if (shape == "brush") {
+    brushMode = true;
+  }
+  if (shape == "rectangle") {
+    rectangleMode = true;
+  }
+  if (shape == "triangle") {
+    triangleMode = true;
+  }
+  if (shape == "rectangle") {
+    circleMode = true;
+  }
+  if (shape == "line") {
+    lineMode = true;
+  }
+  console.log("mode = " + shape);
+}
+
+$(document).ready(function() {
+  $('.color-option').each(function() {
+    var color = $(this)[0].id;
+    $(this).css("background-color", color)
+  })
+  $('.color-option').on('click', function() {
+    currentColor = $(this)[0].id;
+  })
+  $('.make-shape').on('click', function() {
+    var idName = $(this)[0].id
+    var shapeMode = idName.split("-").pop();
+
+    setMode(shapeMode);
+  })
+
+
+})
